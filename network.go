c3628aa123192994e53da2082d537f3baef588f0
Bobby <bobby@bybobby.dev>
2020-04-15 09:21:28 -0400

    Move method

--- network.go
+++ network.go
@@ -1,5 +1,10 @@
 package gochrome
 
+import (
+	"encoding/base64"
+	"fmt"
+)
+
 // SetUserAgent override
 func (t *Tab) SetUserAgent(ua string) {
 	t.NetworkSetUserAgentOverride(ua, "", "")
@@ -8,4 +13,23 @@ func (t *Tab) SetUserAgent(ua string) {
 // SetRequestHeaders override
 func (t *Tab) SetRequestHeaders(ua string, lang string, platform string) {
 	t.NetworkSetUserAgentOverride(ua, lang, platform)
+}
+
+// GetResponseBody for a request
+func (t *Tab) GetResponseBody(id NetworkRequestId) (string, error) {
+	res, err := t.NetworkGetResponseBody(id)
+	if err != nil {
+		return "", fmt.Errorf("Tab.GetResponseBody: %v", err)
+	}
+	var body string
+	if res.Base64Encoded {
+		decoded, err := base64.StdEncoding.DecodeString(res.Body)
+		if err != nil {
+			return "", fmt.Errorf("Tab.GetResponseBody: %v", err)
+		}
+		body = string(decoded)
+	} else {
+		body = res.Body
+	}
+	return body, nil
 }
--- page.go
+++ page.go
@@ -1,30 +1,6 @@
 package gochrome
 
-import (
-	"encoding/base64"
-	"fmt"
-)
-
 // Goto a url
 func (t *Tab) Goto(url string) (PageNavigateReturns, error) {
 	return t.PageNavigate(url, "", "", "")
-}
-
-// GetResponseBody for a request
-func (t *Tab) GetResponseBody(id NetworkRequestId) (string, error) {
-	res, err := t.NetworkGetResponseBody(id)
-	if err != nil {
-		return "", fmt.Errorf("Tab.GetResponseBody: %v", err)
-	}
-	var body string
-	if res.Base64Encoded {
-		decoded, err := base64.StdEncoding.DecodeString(res.Body)
-		if err != nil {
-			return "", fmt.Errorf("Tab.GetResponseBody: %v", err)
-		}
-		body = string(decoded)
-	} else {
-		body = res.Body
-	}
-	return body, nil
 }
