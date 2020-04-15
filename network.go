e1f2097dd4d638a7711cba69f242aaa892827ce2
Bobby <bobby@bybobby.dev>
2020-04-15 09:57:36 -0400

    Add Tab method

--- network.go
+++ network.go
@@ -3,6 +3,8 @@ package gochrome
 import (
 	"encoding/base64"
 	"fmt"
+	"os"
+	"sync"
 )
 
 // SetUserAgent override
@@ -32,4 +34,66 @@ func (t *Tab) GetResponseBody(id NetworkRequestId) (string, error) {
 		body = res.Body
 	}
 	return body, nil
+}
+
+// HTTPResource is a resource the browser has fetched
+type HTTPResource struct {
+	Type    NetworkResourceType
+	Headers NetworkResponse
+	Body    string
+}
+
+// OnResource passes the http response headers and body
+// Document, Stylesheet, Image, Media, Font,
+// Script, TextTrack, XHR, Fetch, EventSource,
+// WebSocket, Manifest, SignedExchange, Ping,
+// CSPViolationReport, Other
+func (t *Tab) OnResource(onResource func(res HTTPResource), types ...NetworkResourceType) error {
+	_, err := t.NetworkEnable(0, 0, 0)
+	if err != nil {
+		return nil
+	}
+	var m sync.Mutex
+	resources := make(map[NetworkRequestId]HTTPResource)
+
+	add := func(id NetworkRequestId, res HTTPResource) {
+		m.Lock()
+		defer m.Unlock()
+		resources[id] = res
+	}
+	del := func(id NetworkRequestId) {
+		m.Lock()
+		defer m.Unlock()
+		delete(resources, id)
+	}
+
+	t.Events.OnNetworkResponseReceived = func(ev NetworkResponseReceivedEvent) {
+		// fmt.Printf("Response: %s (%s)\n", ev.RequestId, ev.Response["url"])
+		for _, tt := range types {
+			if ev.Type == tt {
+				add(ev.RequestId, HTTPResource{
+					Type:    ev.Type,
+					Headers: ev.Response,
+				})
+			}
+		}
+	}
+
+	t.Events.OnNetworkLoadingFinished = func(ev NetworkLoadingFinishedEvent) {
+		// fmt.Printf("Loaded: %s\n", ev.RequestId)
+		if r, ok := resources[ev.RequestId]; ok {
+			del(ev.RequestId)
+			body, err := t.GetResponseBody(ev.RequestId)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Tab.OnResource: %v\n", err)
+			}
+			onResource(HTTPResource{
+				Type:    r.Type,
+				Headers: r.Headers,
+				Body:    body,
+			})
+		}
+	}
+
+	return nil
 }
