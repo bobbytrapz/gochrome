799100da2c89965a79cb524bd3b7434691aec198
Bobby <bobby@bybobby.dev>
2020-04-16 07:26:59 -0400

    Add Tab method

--- page.go
+++ page.go
@@ -4,3 +4,8 @@ package gochrome
 func (t *Tab) Goto(url string) (PageNavigateReturns, error) {
 	return t.PageNavigate(url, "", "", "")
 }
+
+// Close a tab; just calls Tab.PageClose
+func (t *Tab) Close() {
+	t.PageClose()
+}
