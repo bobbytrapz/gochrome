7f00d9bf94891cb817ca0a9bae392ff0d549388a
Bobby <bobby@bybobby.dev>
2021-01-02 14:49:27 -0500

    Add error handling

---
+++ evaluate.go
@@ -0,0 +1,15 @@
+package gochrome
+
+func (t *Tab) Evaluate(js string) (RuntimeEvaluateReturns, error) {
+	r, err := t.RuntimeEvaluate(js, "", false, false, 0, false, false, true, true, false, 0, false, true)
+
+	if err != nil {
+		Log("Tab.Evaluate: error: %q", err)
+	}
+
+	if r.Result != nil {
+		Log("Tab.Evaluate: type: %q description: %q", r.Result["type"], r.Result["description"])
+	}
+
+	return r, err
+}
