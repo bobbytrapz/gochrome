57a29d848ea87dfb465afc0917e5549032444695
Bobby <bobby@bybobby.dev>
2021-01-06 06:01:49 -0500

    Refactor

--- page.go
+++ page.go
@@ -1,5 +1,11 @@
 package gochrome
 
+import (
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+)
+
 // Goto a url
 func (t *Tab) Goto(url string) (PageNavigateReturns, error) {
 	return t.PageNavigate(url, "", "", "", "")
@@ -7,5 +13,47 @@ func (t *Tab) Goto(url string) (PageNavigateReturns, error) {
 
 // Close a tab; just calls Tab.PageClose
 func (t *Tab) Close() {
-	t.PageClose()
+	_, _ = t.PageClose()
+}
+
+// Screenshot captures page as png
+// uses Page.captureScreenshot
+func (t *Tab) Screenshot(saveAs string) error {
+	res, err := t.PageCaptureScreenshot("png", 0, nil, true)
+	if err != nil {
+		return fmt.Errorf("Tab.Screenshot: %w", err)
+	}
+
+	// decode
+	img, err := base64.StdEncoding.DecodeString(res.Data)
+	if err != nil {
+		Log("Tab.Screenshot: %s", err)
+		return err
+	}
+
+	// save
+	if err := ioutil.WriteFile(saveAs, img, 0644); err != nil {
+		Log("Tab.Screenshot: %s", err)
+		return err
+	}
+
+	return nil
+}
+
+// Snapshot page in mhtml format
+// uses Page.captureSnapshot
+func (t *Tab) Snapshot(saveAs string) error {
+	res, err := t.PageCaptureSnapshot("mhtml")
+	if err != nil {
+		return fmt.Errorf("Tab.Snapshot: %w", err)
+	}
+
+	// save
+	if err := ioutil.WriteFile(saveAs, []byte(res.Data), 0644); err != nil {
+		Log("Tab.Snapshot: %s", err)
+	} else {
+		Log("Tab.Snapshot: %s", saveAs)
+	}
+
+	return nil
 }
